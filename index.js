f7888a6643b8205bc16d3b3fef0866d98f3e0fde
agustin yutas de lorenzo <agusyutas@gmail.com>
2023-12-12 21:11:32 -0300

    3-entrega

--- index.js
+++ index.js
@@ -108,3 +108,163 @@ let gabinetes = ["Gabinete Corsair Obsidian","Gabinete Kolink Inspire","Gabinete
 let indice = gabinetes.indexOf("Gabinete Kolink Void");
 console.log("el indice del gabinete es: "+ indice);
 
+document.addEventListener('DOMContentLoaded', function () {
+    const modoOscuroBtn = document.getElementById('modoOscuroBtn');
+    const body = document.body;
+    if (localStorage.getItem('modoOscuro') === 'activado') {
+      body.classList.add('dark-mode');
+    }
+    modoOscuroBtn.addEventListener('click', function () {
+      body.classList.toggle('dark-mode');
+      if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('modoOscuro', 'activado');
+      } else {
+        localStorage.removeItem('modoOscuro');
+      }
+    });
+  });
+
+
+  class Producto {
+    constructor(id, nombre, precio, img) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+        this.cantidad = 1;
+    }
+}
+
+const gabinete = new Producto(1, "Gabinete Corsair Obsidian", 60000, "./img/gabinete.jpg");
+const gabinete2 = new Producto(2, "Gabinete Kolink Inspire", 80000, "./img/gabinete2.jpg");
+const gabinete3 = new Producto(3, "Gabinete Kolink Void", 100000, "./img/gabinete3.jpg");
+const gabinete4 = new Producto(4, "Gabinete Kolink Observatory", 130000, "./img/gabinete4.jpg");
+const gabinete5 = new Producto(5, "Gabinete Corsair Crystal", 75000, "./img/gabinete5.jpg");
+const gabinete6 = new Producto(6, "Gabinete Cooler Master", 90000, "./img/gabinete6.jpg");
+
+const PRODUCTOS = [gabinete, gabinete2, gabinete3, gabinete4, gabinete5, gabinete6];
+
+let carrito = [];
+
+const contenedorProductos = document.getElementById("contenedorProductos");
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const verCarrito = document.getElementById("verCarrito");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const guardarCarritoEnLocalStorage = () => {
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
+    const cargarCarritoDesdeLocalStorage = () => {
+        const carritoGuardado = localStorage.getItem("carrito");
+        if (carritoGuardado) {
+            carrito = JSON.parse(carritoGuardado);
+            mostrarCarritoEnDOM();
+        }
+    };
+
+    const mostrarProductos = () => {
+        contenedorProductos.innerHTML = "";
+        PRODUCTOS.forEach(producto => {
+            const card = document.createElement("div");
+            card.innerHTML = `
+                <div class="card">
+                    <img src="${producto.img}" class="card-img-tom imgProducto"/>
+                    <div class="card-body">
+                        <h2>${producto.nombre}</h2>
+                        <p>${producto.precio}</p>
+                        <button class="btn colorBoton" id="boton${producto.id}">agregar producto</button>
+                    </div>
+                </div>
+            `;
+            contenedorProductos.appendChild(card);
+            const BOTON = document.getElementById(`boton${producto.id}`);
+            BOTON.addEventListener("click", () => {
+                agregarAlCarrito(producto.id);
+            });
+        });
+    };
+
+    const agregarAlCarrito = (id) => {
+        const productoEnCarrito = carrito.find(producto => producto.id === id);
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad++;
+        } else {
+            const producto = { ...PRODUCTOS.find(producto => producto.id === id) };
+            carrito.push(producto);
+        }
+        console.log("Array de objetos:", PRODUCTOS);
+        console.log("Producto agregado al carrito:", carrito.find(producto => producto.id === id));
+        console.log("Cantidad total de productos en el carrito:", calcularCantidadTotal());
+
+        guardarCarritoEnLocalStorage();
+        mostrarCarritoEnDOM();
+    };
+
+    const calcularCantidadTotal = () => {
+        return carrito.reduce((total, producto) => total + producto.cantidad, 0);
+    };
+    const mostrarCarritoEnDOM = () => {
+        if (!contenedorCarrito) {
+            console.error("Elemento 'contenedorCarrito' no encontrado");
+            return;
+        }
+        contenedorCarrito.innerHTML = "";
+        carrito.forEach(producto => {
+            const card = document.createElement("div");
+            card.innerHTML = `
+                <div class="card">
+                    <img src="${producto.img}" class="card-img-tom imgProducto"/>
+                    <div class="card-body">
+                        <h2>${producto.nombre}</h2>
+                        <p>${producto.precio}</p>
+                        <p>${producto.cantidad}</p>
+                        <button class="btn colorBoton" id="eliminar${producto.id}">eliminar producto</button>
+                    </div>
+                </div>
+            `;
+            contenedorCarrito.appendChild(card);
+
+ const BOTON_ELIMINAR = document.getElementById(`eliminar${producto.id}`);
+     if (BOTON_ELIMINAR) {
+            BOTON_ELIMINAR.addEventListener("click", () => {
+                eliminarDelCarrito(producto.id);
+                });
+            } 
+    else {
+        console.error(`Botón 'eliminar${producto.id}' no encontrado`);
+        }
+        });
+        carrito.forEach(producto => {
+            console.log("Producto en el carrito:", producto);
+        });
+    };
+
+const eliminarDelCarrito = (id) => {
+        carrito = carrito.filter(producto => producto.id !== id);
+        guardarCarritoEnLocalStorage();
+        mostrarCarritoEnDOM();
+    };
+    if (verCarrito) {
+        verCarrito.addEventListener("click", () => {
+            contenedorCarrito.classList.toggle("visible");
+            mostrarCarritoEnDOM();
+        });
+    } 
+    else {
+        console.error("Elemento 'verCarrito' no encontrado");
+    }
+
+    mostrarProductos();
+    cargarCarritoDesdeLocalStorage();
+});
+
+
+
+
+
+  
+
+
+
+
+
